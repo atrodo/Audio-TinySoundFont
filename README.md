4e75ab70b61d582bed31dfaf9ad9e7dd71ef757f
Jon Gentle <atrodo@atrodo.org>
2020-12-04 12:04:51 -0500

    Inital version

---
+++ README.md
@@ -0,0 +1,25 @@
+# NAME
+
+Audio::TinySoundFont - Blah blah blah
+
+# SYNOPSIS
+
+    use Audio::TinySoundFont;
+
+# DESCRIPTION
+
+Audio::TinySoundFont is
+
+# AUTHOR
+
+Jon Gentle &lt;cpan@atrodo.org>
+
+# COPYRIGHT
+
+Copyright 2020- Jon Gentle
+
+# LICENSE
+
+This is free software. You may redistribute copies of it under the terms of the Artistic License 2 as published by The Perl Foundation.
+
+# SEE ALSO
---
+++ lib/Audio/TinySoundFont.pm
@@ -0,0 +1,38 @@
+package Audio::TinySoundFont;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Audio::TinySoundFont - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Audio::TinySoundFont;
+
+=head1 DESCRIPTION
+
+Audio::TinySoundFont is
+
+=head1 AUTHOR
+
+Jon Gentle E<lt>cpan@atrodo.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2020- Jon Gentle
+
+=head1 LICENSE
+
+This is free software. You may redistribute copies of it under the terms of the Artistic License 2 as published by The Perl Foundation.
+
+=head1 SEE ALSO
+
+=cut
